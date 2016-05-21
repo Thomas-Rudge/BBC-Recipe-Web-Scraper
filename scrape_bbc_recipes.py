c2371e7e28d03409f9eaf75fd3fdda533d2c35c0
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-05-21 18:07:58 +0530

    Minor update
    Script now checks whether a recipe html file exists firsts before trying
    to create it. For instances where the process fails or is interrupted
    midway.

--- scrape_bbc_recipes.py
+++ scrape_bbc_recipes.py
@@ -1,7 +1,7 @@
 #! python3
-## WARNING ## WARNING ## WARNING ## WARNING ## WARNING ##
-## This script will create a directory with over 11,000##
-## html pages in the directory it is executed from!    ##
+# WARNING ## WARNING ## WARNING ## WARNING ## WARNING ##
+# This script will create a directory with over 11,000##
+# html pages in the directory it is executed from!    ##
 # -------------------------------------------------------------------------------
 # Name:        BBC Food Recipes & Techniques
 # Purpose:     Grab all the recipes from the BBC food website and
@@ -32,21 +32,21 @@ def add_data_to_indexhtml():
     '''
     Add the search data to the search.html search_data div.
     '''
-    ## Check the necessary files are present.
+    # Check the necessary files are present.
     if not os.path.isfile('search.html'):
         raise Exception("Couldn't find the search.html file!")
     elif not os.path.isfile('temp_data.txt'):
         raise Exception("Couldn't find the temp_data.txt file!")
 
-    ## Get the search.html where the data will be inserted
+    # Get the search.html where the data will be inserted
     with open('search.html', 'r', encoding='utf-8') as f:
         html_data = f.read()
 
-    ## Get the index where the data should be inserted
+    # Get the index where the data should be inserted
     insertion_index = html_data.find('<div id=search_data>')
 
-    ## Add the search data to the html file
-    ## The read data is sliced because the last character will be a delimiter
+    # Add the search data to the html file
+    # The read data is sliced because the last character will be a delimiter
     with open('temp_data.txt', 'r') as data_file:
         with open('search.html', 'w', encoding='utf-8') as html_file:
             html_file.write(html_data[:insertion_index + 20] +
@@ -58,7 +58,7 @@ def get_sitemap():
     '''
     Get the BBC Food sitemap and save it to a local file.
     '''
-    ## If the sitemap already exists, don't bother getting it again
+    # If the sitemap already exists, don't bother getting it again
     if os.path.isfile('bbc_sitemap.txt'):
         return
 
@@ -77,7 +77,7 @@ def get_sitemap():
 
     sitemap = bs4.BeautifulSoup(page.text, 'xml')
 
-    ## Write the recipe urls to a text file
+    # Write the recipe urls to a text file
     with open('bbc_sitemap.txt', 'w') as f:
         for line in sitemap.find_all('loc'):
             for string in line.stripped_strings:
@@ -107,7 +107,7 @@ def get_stylesheets():
     '''
     Get the stylesheets needed to display the pages properly.
     '''
-    ## Get the first URL from the sitemap (assume all the pages share styles)
+    # Get the first URL from the sitemap (assume all the pages share styles)
     with open('bbc_sitemap.txt', 'r') as f:
         url = f.readline()
     url = url.strip('\n')
@@ -131,15 +131,16 @@ def get_stylesheets():
     soup = bs4.BeautifulSoup(page.text, 'lxml')
     sheets = []
 
-    ## Get the sheets' urls
+    # Get the sheets' urls
     for link in soup.find_all('link'):
         if 'stylesheet' in link.get('rel'):
             sheets.append(link.get('href'))
 
-    ## Now get and save the sheets
+    # Now get and save the sheets
     for link in sheets:
         link = 'http:' + link if link.startswith('//') else link
         sheet = requests.get(link)
+
         try:
             sheet.raise_for_status()
         except requests.RequestException:
@@ -158,92 +159,95 @@ def save_pages(css_links):
     Grab every web page in the BBC Food sitemap and
     save it as a local html file.
     '''
-    ## Build the new header data
+    # Build the new header data
     for i, link in enumerate(css_links):
         css_links[i] = os.path.join('css', link.split('/')[-1])
 
-    ## Create a file that will hold each page and key information
+    # Create a file that will hold each page and key information
     with open('temp_data.txt', 'w') as f:
         f.write('')
 
-    ## Cycle through the sitemap grabbing each recipe
+    # Cycle through the sitemap grabbing each recipe
     with open('bbc_sitemap.txt', 'r') as f:
         for line in f.readlines():
             line = line.strip('\n')
-            page = requests.get(line)
-
-            try:
-                page.raise_for_status()
-            except requests.RequestException:
-                continue
-
-            soup = bs4.BeautifulSoup(page.text, 'lxml')
-
-            ## Update the data file
-            data = [soup.find('h1', class_='content-title__text'),
-                    '',
-                    soup.find('div', class_='recipe-ingredients')
-                    ]
-
-            for i, datum in enumerate(data):
-                if data[i]:
-                    data[i] = datum.text.replace('\n', '')
-                else:
-                    data[i] = ''
-
-            with open('temp_data.txt', 'a', encoding='utf-8') as tempfile:
-                tempfile.write(line.split('/')[-1] + '~:]' + str(data)[1:-1].upper() + '~:]')
-
-            ## Update the head tag
-            soup.head.clear()
-            for link in css_links:
-                new_link = soup.new_tag('link')
-                new_link.attrs['rel'] = 'stylesheet'
-                new_link.attrs['href'] = link
-                soup.head.append(new_link)
-
-            ## Remove unwanted elements
-            soup.header.decompose()
-            decomp = [soup.find('div', class_='main-menu'),
-                      soup.find('div', class_='food-wrapper'),
-                      soup.find('div', class_='recipe-finder-link__wrap'),
-                      soup.find('div', class_='grid-list-wrapper'),
-                      soup.find('div', class_='recipe-actions'),
-                      soup.find('div', class_='recipe-quick-links'),
-                      soup.find('div', class_='recipe-extra-information__wrapper'),
-                      soup.find('div', id='recipe-finder__box'),
-                      soup.find('div', id='orb-footer'),
-                      soup.find('div', id='blq-global'),
-                      soup.find('a', class_='chef__image-link')]
-
-            for noscript in soup.find_all('noscript'):
-                decomp.append(noscript)
-            for div in soup.find_all('div', class_='recipe-media'):
-                decomp.append(div)
-            for div in soup.find_all('div', class_='bbccom_display_none'):
-                decomp.append(div)
-            for script in soup.find_all('script'):
-                decomp.append(script)
-
-            for ele in decomp:
-                if ele:
-                    ele.decompose()
-
-            ## Convert anchor tags into spans (links into text)
-            tags = []
-            for tag in soup.find_all('a'):
-                tags.append(tag)
-            for tag in tags:
-                new_tag = soup.new_tag('span')
-                new_tag.string = tag.text
-                tag.replace_with(new_tag)
-
-            ## Convert the soup back into html and save it to file
-            html = soup.prettify()
-
             filepath = os.path.join('BBC_Food_Repo', line.split('/')[-1] + '.html')
-            with open(filepath, 'w', encoding='utf-8-sig') as html_page:
-                html_page.write(html)
+
+            # Don't get pages that have already been collected
+            if not os.path.isfile(filepath):
+                page = requests.get(line)
+
+                try:
+                    page.raise_for_status()
+                except requests.RequestException:
+                    continue
+
+                soup = bs4.BeautifulSoup(page.text, 'lxml')
+
+                # Update the data file
+                data = [soup.find('h1', class_='content-title__text'),
+                        '',
+                        soup.find('div', class_='recipe-ingredients')
+                        ]
+
+                for i, datum in enumerate(data):
+                    if data[i]:
+                        data[i] = datum.text.replace('\n', '')
+                    else:
+                        data[i] = ''
+
+                with open('temp_data.txt', 'a', encoding='utf-8') as tempfile:
+                    tempfile.write(line.split('/')[-1] + '~:]' + str(data)[1:-1].upper() + '~:]')
+
+                # Update the head tag
+                soup.head.clear()
+                for link in css_links:
+                    new_link = soup.new_tag('link')
+                    new_link.attrs['rel'] = 'stylesheet'
+                    new_link.attrs['href'] = link
+                    soup.head.append(new_link)
+
+                # Remove unwanted elements
+                soup.header.decompose()
+                decomp = [soup.find('div', class_='main-menu'),
+                          soup.find('div', class_='food-wrapper'),
+                          soup.find('div', class_='recipe-finder-link__wrap'),
+                          soup.find('div', class_='grid-list-wrapper'),
+                          soup.find('div', class_='recipe-actions'),
+                          soup.find('div', class_='recipe-quick-links'),
+                          soup.find('div', class_='recipe-extra-information__wrapper'),
+                          soup.find('div', id='recipe-finder__box'),
+                          soup.find('div', id='orb-footer'),
+                          soup.find('div', id='blq-global'),
+                          soup.find('a', class_='chef__image-link')]
+
+                for noscript in soup.find_all('noscript'):
+                    decomp.append(noscript)
+                for div in soup.find_all('div', class_='recipe-media'):
+                    decomp.append(div)
+                for div in soup.find_all('div', class_='bbccom_display_none'):
+                    decomp.append(div)
+                for script in soup.find_all('script'):
+                    decomp.append(script)
+
+                for ele in decomp:
+                    if ele:
+                        ele.decompose()
+
+                # Convert anchor tags into spans (links into text)
+                tags = []
+                for tag in soup.find_all('a'):
+                    tags.append(tag)
+                for tag in tags:
+                    new_tag = soup.new_tag('span')
+                    new_tag.string = tag.text
+                    tag.replace_with(new_tag)
+
+                # Convert the soup back into html and save it to file
+                html = soup.prettify()
+
+                with open(filepath, 'w', encoding='utf-8-sig') as html_page:
+                    html_page.write(html)
 
 
 def main():
